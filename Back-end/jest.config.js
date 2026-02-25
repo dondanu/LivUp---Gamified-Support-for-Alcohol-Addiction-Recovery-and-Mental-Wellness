0aca7dc472d68b21fa689c19d6c692701c3b9e97
K DANUSAN <danusan.official@gmail.com>
2026-02-25 23:13:49 +0530

    update config,js

---
+++ Back-end/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/config/initDatabase.js',
+    '!**/node_modules/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+  testMatch: [
+    '**/__tests__/**/*.js',
+    '**/?(*.)+(spec|test).js',
+  ],
+  verbose: true,
+  testTimeout: 10000,
+};
